426f7b2ff71af1bdba63e6079b21a1191868869c
roman <romantiholiz154@gmail.com>
2026-05-07 16:28:23 +0300

    new commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -147,6 +147,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "frontend",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
