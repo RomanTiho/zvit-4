455aa29d1b4129784bb874554c398e835422dd78
roman <romantiholiz154@gmail.com>
2026-05-07 17:57:35 +0300

    fix: correct settings module path and switch to WSGI for Render deploy

--- my_project/wsgi.py
+++ my_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
 
 application = get_wsgi_application()
 
